da45dfcb990dccbed5be35ebe5bfc63c6f1ff985
Amitesh Awasthi <156991418+amitesh-awasthi-313@users.noreply.github.com>
2025-06-01 18:32:13 +0530

    Add files via upload

---
+++ Leetcode/01_Twosum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i , j in enumerate(nums):
+            diff = target - j 
+            if diff in d :
+                return ([d[diff] , i])
+            d[j] = i 
